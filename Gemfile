8b5027b578262070834e63dcc7c7655e438c6046
Yann Prono <yann.prono@nist.gov>
2020-09-26 17:27:09 -0400

    almost done

--- Gemfile
+++ Gemfile
@@ -28,4 +28,5 @@ gem 'jekyll', '~> 4.0'
 # Performance-booster for watching directories on Windows
 gem "wdm", "~> 0.1.1", :install_if => Gem.win_platform?
 
-gem "jekyll-image-size", "~> 1.2"
+
+gem "jekyll-responsive-image", "~> 1.5"
--- _plugins/urls.rb
+++ _plugins/urls.rb
@@ -1,9 +1,9 @@
 module Jekyll
   module Paintings
     def painting(input)
-      return File.join("/assets/images/", input)
+      return File.join("assets/images/", input)
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::Paintaings)
+Liquid::Template.register_filter(Jekyll::Paintings)
