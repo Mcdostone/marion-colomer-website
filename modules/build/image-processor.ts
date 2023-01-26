4369adc2d4990b1ee0eaea225a428feed15f51e7
Yann Prono <yann.prono@telecomnancy.net>
2023-01-26 15:13:03 -0500

    update colorthief

--- modules/build/image-processor.ts
+++ modules/build/image-processor.ts
@@ -1,15 +1,15 @@
-import ColorThief from 'colorthief'
-import path from 'node:path'
-import { promises as fs } from 'node:fs'
 import Image from '@11ty/eleventy-img'
+import { getAverageColor } from 'fast-average-color-node'
+import { promises as fs } from 'node:fs'
+import path from 'node:path'
+
 import { Document } from './document'
 import { Processor } from './processor'
 
 interface ImageData {
   input: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data: any
-  html: string
+  metadata: any
 }
 
 export class ImageProcessor implements Processor {
@@ -18,90 +18,83 @@ export class ImageProcessor implements Processor {
 
   async getPrimaryColor(file: string) {
     try {
-      return await ColorThief.getColor(file).then(this.rgbToHex)
+      return await getAverageColor(file).then((result) => result.hex)
     } catch (error) {
       console.log(error)
       return ''
     }
   }
 
-  rgbToHex(array: Array<number>) {
-    return (
-      '#' +
-      array
-        .map((x) => {
-          const hex = x.toString(16)
-          return hex.length === 1 ? '0' + hex : hex
-        })
-        .join('')
-    )
-  }
-
   async process(document: Document) {
     const images = document.querySelectorAll('img') as NodeListOf<HTMLImageElement>
     for (const image of images) {
-      image.replaceWith(document.createRange().createContextualFragment(await this.hitCacheOr(document, image)))
-    }
-
-    const linksToImages = document.querySelectorAll('a[data-pswp-width]') as NodeListOf<HTMLAnchorElement>
-    for (const link of linksToImages) {
-      link.href = this.cache.get(document.getAbsolutePath(link.href))?.data.jpeg[0].url
+      const data = await this.hitCacheOr(document, image)
+      image.replaceWith(document.createRange().createContextualFragment(await this.generateHTML(data, image)))
+      for (const link of document.querySelectorAll<HTMLAnchorElement>(`[href='${image.src}']`)) {
+        link.href = data.metadata.avif[0].url
+      }
     }
   }
 
-  async hitCacheOr(document: Document, image: HTMLImageElement): Promise<string> {
+  async hitCacheOr(document: Document, image: HTMLImageElement): Promise<ImageData> {
     const imagePath = document.getAbsolutePath(image.src)
     if (!this.cache.has(imagePath)) {
       const data = await this.optimize(document, image)
-      this.toDelete.add(data.input)
+      this.toDelete = this.toDelete.add(data.input)
       this.cache = this.cache.set(data.input, data)
     }
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return this.cache.get(imagePath)!.html
+    return this.cache.get(imagePath)!
   }
 
-  async optimize(document: Document, image: HTMLImageElement): Promise<ImageData> {
+  private async generateHTML(imageData: ImageData, image: HTMLImageElement): Promise<string> {
     // eslint-disable-next-line unicorn/no-array-reduce
     const options = image.getAttributeNames().reduce((accumulator, name) => {
       return { ...accumulator, [name]: image.getAttribute(name) }
     }, {})
-
-    const imagePath = document.getAbsolutePath(image.src)
     options['alt'] = options['alt'] || ''
     const containerOptions = {}
     if (options['class']) {
       containerOptions['class'] = options['class']
       delete options['class']
     }
-    const metadata = await Image(imagePath, {
-      formats: ['jpeg', 'avif'],
-      outputDir: path.resolve(path.dirname(imagePath)),
-      urlPath: path.dirname(document.getUrl(imagePath)),
-      filenameFormat: function (id: string, source: string, width: number, format: string) {
-        const extension = path.extname(source)
-        const name = path.basename(source, extension)
-        return `${name}-${id}.${format}`
-      },
-    })
+    if (options['style']) {
+      containerOptions['style'] = options['style']
+      delete options['style']
+    }
     const imageAttributes = {
       ...options,
       loading: 'lazy',
       decoding: 'async',
     }
-    const markup = await Image.generateHTML(metadata, imageAttributes)
-    const primaryColor = await this.getPrimaryColor(metadata.jpeg[0].outputPath)
+    const markup = await Image.generateHTML(imageData.metadata, imageAttributes)
+    const primaryColor = await this.getPrimaryColor(imageData.metadata.jpeg[0].outputPath)
     if (primaryColor !== '') {
-      containerOptions['style'] = [containerOptions['style'], `--pc: ${primaryColor}`].filter((s) => s !== undefined).join(';')
+      containerOptions['style'] = `${[containerOptions['style'], `--pc: ${primaryColor}`].filter((s) => s !== undefined).join(';')}`
     }
+    return markup.replace(
+      '<picture',
+      `<picture${Object.entries(containerOptions)
+        .map((source) => ` ${source[0]}="${source[1]}"`)
+        .join('')}`
+    )
+  }
+
+  async optimize(document: Document, image: HTMLImageElement): Promise<ImageData> {
+    const imagePath = document.getAbsolutePath(image.src)
+    const metadata = await Image(imagePath, {
+      formats: ['avif', 'jpeg'],
+      outputDir: path.resolve(path.dirname(imagePath)),
+      urlPath: path.dirname(document.getUrl(imagePath)),
+      filenameFormat: function (id, source, width, format) {
+        const extension = path.extname(source)
+        const name = path.basename(source, extension)
+        return `${name}-${id}.${format}`
+      },
+    })
     return {
       input: imagePath,
-      data: metadata,
-      html: markup.replace(
-        '<picture',
-        `<picture${Object.entries(containerOptions)
-          .map((source) => ` ${source[0]}="${source[1]}"`)
-          .join('')}`
-      ),
+      metadata: metadata,
     }
   }
 
